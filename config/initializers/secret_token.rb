9324385bab7f818e991488f837ae43aae5bffb79
Josef Diago <josef.diago@gmail.com>
2012-08-13 19:22:54 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beerminder::Application.config.secret_token = 'fb9e3186b64182d0e736b3edb9dd10ab574a2bdc332b438728a8d40e91ea15a67d31d05b533d7949379cad0591d802bf93e4a76c54a3e344f52e70cd30f55485'
